c93e9815410d20ee0f406ab82d84bfd13a311328
esaki01 <social0430@gmail.com>
2019-02-12 21:45:41 +0900

    [Enhancement] はじめての Django アプリ作成、その5 - テストを作成する

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -2,7 +2,10 @@
 モデルの定義.
 """
 
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
@@ -11,6 +14,10 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
